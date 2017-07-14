88a7b5716a1d2ace61dc10cce623c9beaefbb1e5
Jacek Ławniczak <jacekelgda@gmail.com>
2017-07-14 20:07:22 +0200

    # This is a combination of 4 commits. # This is the 1st commit message: npm package export --closes #4
    # The commit message #2 will be skipped:
    
    #	1.0.1
    
    # The commit message #3 will be skipped:
    
    #	Added bundle.js lib
    
    # The commit message #4 will be skipped:
    
    #	1.0.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Logo from './components/Logo'
+import Logo from './components/Logo/Logo'
 import AppBar from './components/AppBar/AppBar'
 import Tab from './components/Tab/Tab'
 import Tabs from './components/Tab/Tabs'
@@ -6,7 +6,7 @@ import Avatar from './components/Avatar/Avatar'
 import Menu from './components/Menu/Menu'
 import MenuItem from './components/Menu/MenuItem'
 
-export default {
+export {
   Logo,
   AppBar,
   Tab,
