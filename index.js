c0ac20831ea15b9ae72e10ba367c540e64239050
Jacek Ławniczak <jacekelgda@gmail.com>
2017-08-18 12:51:55 +0200

    Relative path

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var app = express();
 app.use(express.static(__dirname + "/styleguide/index.html"));
 
 app.get('/',function(req,res){
-  res.sendFile(__dirname + "/styleguide/index.html");
+  console.log(__dirname + "/app/styleguide/index.html")
+  res.sendFile(__dirname + "/app/styleguide/index.html")
 });
 
-app.use('/build', express.static(__dirname + "/styleguide/build"))
+app.use('/build', express.static(__dirname + "/app/styleguide/build"))
 
-var server = app.listen(process.env.PORT);
+var server = app.listen(process.env.PORT || 8080)
